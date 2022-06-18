47c33988d53690e20cedc36a7acefaced909da8c
adarshsinggh <adarshsinggh@gmail.com>
2022-06-18 19:25:40 +0530

    Fibonacci Series

---
+++ FibonacciSeries.java
@@ -0,0 +1,14 @@
+public class FibonacciSeries{
+	public static void main(String args[]){
+		
+		int n1=0,n2=1,n3,count=10;
+		System.out.print(n1+","+n2);
+		for(int i=2;i<count;++i){
+		n3 = n1+n2;
+		System.out.print(","+n3);
+		n1 = n2;
+		n2 = n3;
+		}		
+	
+	}
+}
