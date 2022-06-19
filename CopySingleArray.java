7c845e0a418b7709fd58913627335b488119fdde
Adarsh Singh <adarshsinggh@gmail.com>
2022-06-19 21:44:37 +0530

    Update CopySingleArray.java

--- CopySingleArray.java
+++ CopySingleArray.java
@@ -1,9 +1,9 @@
 public class CopySingleArray{
 	public static void main(String args[]){
 
-		int a[] = {1,2,6,8,9,3};
+		int[] a = {1,2,6,8,9,3};
 		int len = a.length;
-		int b[] = new int[len];
+		int[] b = new int[len];
 		for(int i = 0;i<a.length;i++){
 			b[i] = a[i];
 			System.out.print(b[i]+",");
