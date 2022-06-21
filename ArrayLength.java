9aa8268937c764fc5ca7d42edfe3df9f48e20c57
Adarsh Singh <adarshsinggh@gmail.com>
2022-06-21 14:23:41 +0530

    ArrayLength

---
+++ ArrayLength.java
@@ -0,0 +1,6 @@
+public class ArrayLength{
+	public static void main(String args[]){
+		int[] a = new int[] {1,8,8,6,7,5,9,7,6};
+		System.out.println("The elements present in array are : "+a.length);
+	}
+}
