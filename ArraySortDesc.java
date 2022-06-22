39a38915d071c9d38ff95a9db5b921f6de52b80a
Adarsh Singh <adarshsinggh@gmail.com>
2022-06-22 10:29:15 +0530

    ArraySortDesc

---
+++ ArraySortDesc.java
@@ -0,0 +1,18 @@
+public class ArraySortDesc{
+	public static void main(String args[]){
+		int arr[] = new int[] {2,8,6,9,72,6};
+		int temp = 0;
+		for(int i=0;i<arr.length;i++){
+			for(int j=i+1;j<arr.length;j++){
+				if(arr[i]<arr[j]){
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				}
+			}
+		}
+		 for(int i =0;i<arr.length;i++){
+			System.out.print(arr[i]+",");
+		 }
+	}
+}
