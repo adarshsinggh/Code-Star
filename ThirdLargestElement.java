1a32703f95299542de7410083c757b16e1c8a9e1
Adarsh Singh <adarshsinggh@gmail.com>
2022-06-22 11:00:18 +0530

    ThirdLargestElement

---
+++ ThirdLargestElement.java
@@ -0,0 +1,17 @@
+public class ThirdLargestElement{
+	public static void main(String args[]){
+		int[] a = new int[] {9,3,4,8,2};
+		int temp = 0;
+			
+		for(int i=0;i<a.length;i++){
+			for(int j=i+1;j<a.length;j++){
+				if(a[i]>a[j]){
+				  temp = a[i];
+			 	  a[i] = a[j];
+				  a[j] = temp;
+				}
+			}		
+		}
+		System.out.println(a[a.length-3]);
+	}
+}
