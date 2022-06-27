e0246edcc1fcb124282ca589a0ef2e0684cf759c
Adarsh Singh <adarshsinggh@gmail.com>
2022-06-27 11:10:34 +0530

    LongestRepeatingSequence

---
+++ LongestRepeatingSequence.java
@@ -0,0 +1,26 @@
+public class LongestRepeatingSequence{
+	public static void main(String args[]){
+		String input = "sadhadgajfgf";
+		String lrs ="";
+		int n = input.length();
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				String x = lcp(input.substring(i,n),input.substring(j,n));
+				if(x.length()>lrs.length()){
+					lrs =x;
+				}	
+			}
+		}
+			System.out.println(lrs);
+	}
+
+	public static String lcp(String s,String t){
+		int n = Math.min(s.length(),t.length());
+		for(int i=0;i<n;i++){
+			if(s.charAt(i)!=t.charAt(i)){
+				return s.substring(0,i);
+			}
+		}
+	return s.substring(0,n);
+	}
+}
