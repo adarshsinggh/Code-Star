0eb7ce1d1b3319c04aef4228c282cf88f2620f15
Adarsh Singh <adarshsinggh@gmail.com>
2022-08-13 14:10:10 +0530

    MultiArrayList

---
+++ MultiAL.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiAL {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        for (int i=0;i<3;i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                list.get(i).add(in.nextInt());
+            }
+        }
+        System.out.println(list);
+    }
+}
